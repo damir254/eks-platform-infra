4abdddbc1add43b56549bc05131a5b0b45ac60e4
Damir <123@gmail.com>
2026-04-22 21:58:25 +0200

    Testing

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -161,3 +161,67 @@ module "github_oidc_app_repo" {
     Environment = var.environment
   }
 }
+
+resource "aws_iam_policy" "argocd_image_updater_ecr_read" {
+  name = "${var.project_name}-${var.environment}-argocd-image-updater-ecr-read"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "ECRAuth"
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "ECRRead"
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchGetImage",
+          "ecr:DescribeImages",
+          "ecr:DescribeRepositories",
+          "ecr:ListImages"
+        ]
+        Resource = [
+          module.ecr.repository_arn,
+          module.frontend_ecr.repository_arn
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "argocd_image_updater" {
+  name = "${var.project_name}-${var.environment}-argocd-image-updater-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "argocd_image_updater_ecr_read" {
+  role       = aws_iam_role.argocd_image_updater.name
+  policy_arn = aws_iam_policy.argocd_image_updater_ecr_read.arn
+}
+
+resource "aws_eks_pod_identity_association" "argocd_image_updater" {
+  cluster_name    = module.eks.cluster_name
+  namespace       = "argocd"
+  service_account = "argocd-image-updater-controller"
+  role_arn        = aws_iam_role.argocd_image_updater.arn
+}
